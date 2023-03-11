a2296ab0fc1b9b32dc9b41fbf0095e65865cd93f
Perry Naseck <git@perrynaseck.com>
2023-03-11 12:34:39 -0500

    fix test wrong HTTP code comment

--- modbus_exporter_test.go
+++ modbus_exporter_test.go
@@ -59,7 +59,7 @@ func TestScrapeHandler(t *testing.T) {
 			name: "module and target",
 			// The exporter won't be able to access the target,
 			// thus, validation should pass (no 400) but scrape should
-			// fail (500). One could stub the exporter itself.
+			// fail (503). One could stub the exporter itself.
 			code: http.StatusServiceUnavailable,
 			config: func() config.Config {
 				c := config.Config{}
